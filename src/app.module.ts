536dbcbb4d6bd8dc3a18d8fe57bc323388e2ecba
Andrew MacKenzie <mr.andrewmackenzie@gmail.com>
2024-09-16 12:18:21 -0400

    Gets a Database Config roughed in (crude)
    - uses Config, but not nicely
    - a few options are left not configurable
      - synchronize & auto-load-models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        dialect: 'postgres', // hardcoded **
+        username: configService.getOrThrow('DB__USERNAME'),
+        password: configService.getOrThrow('DB__PASSWORD'),
+        host: configService.getOrThrow('DB__HOST'),
+        port: configService.getOrThrow('DB__PORT'),
+        database: configService.getOrThrow('DB__DATABASE'),
+        synchronize: false,
+        autoLoadModels: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
