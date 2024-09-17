b261b666e8b8eb6cd3139d64878de38113de6be5
Andrew MacKenzie <mr.andrewmackenzie@gmail.com>
2024-09-17 10:46:50 -0400

    Adds config for the App-Port
    - configurable @ APP_PORT
    - defaults to 3000

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  private readonly logger: Logger = new Logger(AppService.name);
+
+  constructor(private readonly configService: ConfigService) {}
+
+  /**
+   * Gets the Port configured for the Application or a Default Value if no valid value is found.
+   * @returns Application Port
+   */
+  getAppPort() {
+    const PortConfigName = 'APP_PORT';
+    const DefaultAppPort = 3000;
+
+    let appPort: number | undefined;
+
+    try {
+      appPort = this.configService.getOrThrow(PortConfigName);
+    } catch (err) {
+      this.logger.error(err, err.stack);
+    } finally {
+      if (
+        appPort == null ||
+        typeof appPort !== 'number' ||
+        Number.isNaN(appPort)
+      ) {
+        this.logger.warn(
+          `Invalid App-Port configuration value (${appPort}) -- defaulting to ${DefaultAppPort}.`,
+        );
+
+        this.logger.warn(
+          `Please review your configured ${PortConfigName} value.`,
+        );
+
+        appPort = DefaultAppPort;
+      }
+    }
+
+    return appPort;
+  }
   getHello(): string {
     return 'Hello World!';
   }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppService } from './app.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  const appService: AppService = app.get(AppService);
+
+  const appPort = appService.getAppPort();
+
+  const logger: Logger = new Logger('Main');
+  logger.log(`App is listening on port: ${appPort}`);
+
+  await app.listen(appPort);
 }
 bootstrap();
