dcf37fd5d960157156614486f56c04e6b709a5d3
Andrew MacKenzie <mr.andrewmackenzie@gmail.com>
2024-09-17 14:04:40 -0400

    Tries to get Truck entity to work, using Sequelize's TypeScript docs as ref/guide
    - https://sequelize.org/docs/v6/other-topics/typescript/
    - no luck yet

--- src/domain/truck/entities/truck.entity.ts
+++ src/domain/truck/entities/truck.entity.ts
@@ -1,14 +1,17 @@
-import { Column, Model, PrimaryKey, Table } from 'sequelize-typescript';
+import { Table } from 'sequelize-typescript';
+import { Model, InferAttributes, InferCreationAttributes, ForeignKey, NonAttribute, CreationOptional } from 'sequelize';
+import { Site } from 'src/domain/site/entities/site.entity';
 
 @Table({ tableName: 'trucks', timestamps: false })
-export class Truck extends Model {
-  @PrimaryKey
-  @Column
-  id: number;
+export class Truck extends Model<
+  InferAttributes<Truck>,
+  InferCreationAttributes<Truck>
+> {
+  declare id: CreationOptional<number>;
 
-  @Column
-  license: string;
+  declare license: string;
 
-  @Column
-  site_id: number;
+  declare siteId: ForeignKey<Site['id']>;
+
+  declare site: NonAttribute<Site>;
 }
