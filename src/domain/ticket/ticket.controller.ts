7588bf93c32cbcfa74b5da129f899daabfad6362
Andrew MacKenzie <mr.andrewmackenzie@gmail.com>
2024-09-18 11:18:03 -0400

    Removes the root `ticket` off of our Ticket-Controller
    - changes our fetch endpoint to `/tickets`
    - this ensures our bulk-creation endpoint will not get in the way of a likely future endpoint of `/tickets/{id}`

--- src/domain/ticket/ticket.controller.ts
+++ src/domain/ticket/ticket.controller.ts
@@ -3,35 +3,53 @@ import {
   Get,
   Post,
   Body,
-  Patch,
-  Param,
-  Delete,
   UseInterceptors,
   ClassSerializerInterceptor,
+  Query,
+  BadRequestException,
+  ParseArrayPipe,
 } from '@nestjs/common';
 import { CreateTruckTickets } from './dto/create-truck-tickets.dto';
 import { TicketCreationService } from './services/creation/ticket-creation.service';
 import { TicketFetchingService } from './services/fetching/ticket-fetching.service';
+import { TicketFetchArgs } from './services/fetching/ticket-fetch.args';
 
-@Controller('ticket')
+@Controller()
 export class TicketController {
   constructor(
     private readonly ticketCreationService: TicketCreationService,
     private readonly ticketFetchingService: TicketFetchingService,
   ) {}
 
-  @Get()
+  @Get('tickets')
   @UseInterceptors(ClassSerializerInterceptor)
-  async getAll() {
-    const startDate = new Date('2024-09-17T14:40:18.000Z');
-    const endDate = new Date('2024-09-17T14:47:18.000Z');
-    return await this.ticketFetchingService.fetchAllBy({
-      siteIds: [1, 11],
-      dateRange: {
+  async getAll(
+    @Query('siteIds', new ParseArrayPipe({ items: Number, separator: ',' }))
+    siteIds: number[],
+    @Query('startDate')
+    startDate: Date,
+    @Query('endDate')
+    endDate: Date,
+  ) {
+    const fetchParams: TicketFetchArgs = {};
+    if (siteIds != null) {
+      fetchParams.siteIds = siteIds;
+    }
+
+    if (startDate != null || endDate != null) {
+      if (startDate == null || endDate == null) {
+        throw new BadRequestException(
+          'Both Start-Date & End-Date must by provided.',
+        );
+      }
+
+      fetchParams.dateRange = {
         start: startDate,
         end: endDate,
-      },
-    });
+      };
+    }
+
+    return await this.ticketFetchingService.fetchAllBy(fetchParams);
   }
 
   @Post('create-truck-tickets')
