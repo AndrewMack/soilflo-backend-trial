3836d195d772979f81e42981e7630520bb163827
Andrew MacKenzie <mr.andrewmackenzie@gmail.com>
2024-09-23 15:24:25 -0400

    Adds a way to run data-seeds at bootup
    - hoping this makes it easier for folks starting this project for the first time
    - later, should turn this into a simple npm-run script so we don't have to bother with .env vars

--- src/domain/site/site.module.ts
+++ src/domain/site/site.module.ts
@@ -3,10 +3,12 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { Site } from './entities/site.entity';
 import { SiteController } from './site.controller';
 import { SiteFetchingService } from './services/site-fetching.service';
+import { SiteSeedingService } from './services/site-seeding.service';
 
 @Module({
   imports: [SequelizeModule.forFeature([Site])],
-  providers: [SiteFetchingService],
+  providers: [SiteFetchingService, SiteSeedingService],
   controllers: [SiteController],
+  exports: [SiteSeedingService],
 })
 export class SiteModule {}
--- src/domain/truck/truck.module.ts
+++ src/domain/truck/truck.module.ts
@@ -3,10 +3,12 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { Truck } from './entities/truck.entity';
 import { TruckController } from './truck.controller';
 import { TruckFetchingService } from './services/truck-fetching.service';
+import { TruckSeedingService } from './services/truck-seeding.service';
 
 @Module({
   imports: [SequelizeModule.forFeature([Truck])],
-  providers: [TruckFetchingService],
+  providers: [TruckFetchingService, TruckSeedingService],
   controllers: [TruckController],
+  exports: [TruckSeedingService],
 })
 export class TruckModule {}
